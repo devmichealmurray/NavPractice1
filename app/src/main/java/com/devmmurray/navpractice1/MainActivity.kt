8ae0b23dec3f2426d1e9249ba663922de3c14903
Micheal Murray <michealmurray16@gmail.com>
2020-06-03 10:17:59 -0500

    Initial commit

---
+++ app/src/main/java/com/devmmurray/navpractice1/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.devmmurray.navpractice1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.devmmurray.navpractice1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
